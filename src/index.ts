a4b92bd8caaa47c83b592989454721dc36ced967
Davaakhuu Undarmaa <v25y0009@oita-u.ac.jp>
2025-05-26 07:23:24 +0000

    honoでOpen

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 // index.ts
+
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import {zValidator } from '@hono/zod-validator'
+import {z, createRoute, OpenAPIHono } from '@hono/zod-openapi'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(logger())
 
-app.get('/api', (c) => {
-  const query = c.req.query('name')
-  return c.json({ message: `Hello ${query}!` })
-})
+app.post('/api', 
+  zValidator ('form', z.object({body: z.string()})) ,// Хариулт буцаах хэсэг нэмэв
+  (c) => {
+    const validated = c.req.valid('form')
+    return c.text ('POST /api')
+  }
+)
 
 export default app
+
