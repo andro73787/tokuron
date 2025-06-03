518bbc7b6ac0547805c529e49e2c461f501d0235
Davaakhuu Undarmaa <v25y0009@oita-u.ac.jp>
2025-06-03 09:56:27 +0000

    “honoでOpenAPIの確認”

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,37 @@
 
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
-import {zValidator } from '@hono/zod-validator'
-import {z, createRoute, OpenAPIHono } from '@hono/zod-openapi'
-
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
+import { swaggerUI } from '@hono/swagger-ui'
 const app = new Hono()
 
 app.use(logger())
 
-app.post('/api', 
-  zValidator ('form', z.object({body: z.string()})) ,// Хариулт буцаах хэсэг нэмэв
+// Add the POST /api route
+app.post(
+  '/api',
+  zValidator('form', z.object({ body: z.string() })),
   (c) => {
     const validated = c.req.valid('form')
-    return c.text ('POST /api')
+    return c.text('POST /api')
   }
 )
 
+// Check the h.page
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+// /api test with query
+app.get('/api', (c) => {
+  const query = c.req.query('name')
+  return c.json({ message: `Hello ${query}!` })
+})
+
+// 404 not found 
+app.notFound((c) => c.text('Custom 404 Message', 404))
+
+// Export do
 export default app
 
