7b37df2064283d126cb4f3930a54a8b2b97a4cc8
Davaakhuu Undarmaa <v25y0009@oita-u.ac.jp>
2025-06-13 04:21:34 +0000

    不要なコードの削除

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import { OpenAPIHono, createRoute } from '@hono/zod-openapi';
 import { Hono } from 'hono';
-import { logger } from 'hono/logger';
 import { zValidator } from '@hono/zod-validator';
 import { z } from 'zod';
 import { swaggerUI } from '@hono/swagger-ui';
 
 // Hono-г OpenAPIHono change
-const app = new OpenAPIHono();
+//const app = new OpenAPIHono();
 
-app.use(logger());
 
 // POST /api роут (OpenAPI )
 app.post(
@@ -21,28 +19,6 @@ app.post(
 );
 
 // GET /api - OpenAPI-р change
-app.openapi(
-createRoute({
-  method: 'get',
-  path: '/api',
-  request: {
-    params: z.object({name: z.string().optional() })
-  },
-  responses: {
-    200: {
-      description: 'Respond a hello message',
-      content: {
-        'application/json': {schema: z.object({message: z.string() })}
-        
-      }
-    }
-  }
-}),
-(c) => { 
-const query = c.req.query('name'); 
-  return c.json({ message: `Hello ${query}!` })
-})
-// Root
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
