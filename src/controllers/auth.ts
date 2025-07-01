d1474a425d35813b1ea116b2e073cbd67115143d
Davaakhuu Undarmaa <v25y0009@oita-u.ac.jp>
2025-07-01 09:00:10 +0000

    データベースとORM

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -38,9 +38,8 @@ authApp.post(LOGIN_ROUTE, async (c) => {
        }
     const { JWT_SECRET } = env<{ JWT_SECRET: string }, typeof c>(c);
     const token = await sign ({ ...fulluser, password: undefined }, JWT_SECRET );
-    const token = await sign({ email: email, password: undefined }, JWT_SECRET);
         return c.json({ token });
     });
-    
+
     return authApp ;   
 }
